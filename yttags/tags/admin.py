6fba11de250e43a1449e3aa55e148c50409bf0b4
Matt Young <mmyoung979@gmail.com>
2020-07-28 22:28:02 -0500

    Register Video Object

--- yttags/tags/admin.py
+++ yttags/tags/admin.py
@@ -1,2 +1,11 @@
 # Django imports
 from django.contrib import admin
+
+# Project imports
+from .models import Video
+
+
+# Register Video model
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    pass
